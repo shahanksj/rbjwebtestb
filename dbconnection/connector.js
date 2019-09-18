80a80fdd4404ae64569472fbea8bc40e82016ad9
shahanksj <55353900+shahanksj@users.noreply.github.com>
2019-09-17 22:34:39 -0400

    nine

--- dbconnection/connector.js
+++ dbconnection/connector.js
@@ -7,6 +7,7 @@ var pool= mysql.createPool({
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port:"80",
   //  queueLimit: 30,
     acquireTimeout: 1000000,
     multipleStatements: true
